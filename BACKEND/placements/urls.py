4f5ecdceeb8aa03c546062c2c92f35499c6f39c3
Akello Beatrice <bakello475@gmail.com>
2026-04-16 00:52:51 +0300

    Week 4: Added URL routes for placements app

---
+++ BACKEND/placements/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PlacementListCreateView.as_view(), name='placement-list-create'),
+    path('<int:pk>/', views.PlacementDetailView.as_view(), name='placement-detail'),
+]
