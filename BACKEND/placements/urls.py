e045068f9ccf44c4c3f518cf23b0fdd7a461950a
Akello Beatrice <bakello475@gmail.com>
2026-04-16 11:04:19 +0300

    : Updated placements URLs to include status update endpoint

--- BACKEND/placements/urls.py
+++ BACKEND/placements/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.PlacementListCreateView.as_view(), name='placement-list-create'),
     path('<int:pk>/', views.PlacementDetailView.as_view(), name='placement-detail'),
+    path('<int:pk>/status/', views.PlacementStatusUpdateView.as_view(), name='placement-status'),
 ]
