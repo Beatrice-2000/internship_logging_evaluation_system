0d6c480a18e48d02eb15878ae6d2a436d8543298
kkenan26 <kkenan2420@gmail.com>
2026-04-22 11:15:41 +0300

    Start on status tracking

--- BACKEND/logbook/models.py
+++ BACKEND/logbook/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import CustomUser
 from placements.models import InternshipPlacement
+from django.utils import timezone
 
 
 class WeeklyLog(models.Model):
@@ -35,11 +36,34 @@ class WeeklyLog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    reviewed_at = models.DateTimeField(blank= True, null =True)
+    approved_at =models.DateTimeField(blank= True, null= True)
+
     class Meta:
         unique_together = ['student', 'placement', 'week_number']
 
     def __str__(self):
         return f"{self.student.username} - Week {self.week_number}"
+
+    def save(self, *args, **kwargs):
+        #Checks the status of a log and sets the timestamp for the relevant status
+        if self.status == 'submitted' and self.submitted_at is None:
+            self.submitted_at = timezone.now()
+        if self.status =='reviewed' and self.reviewed_at is None:
+            self.reviewed_at== timezone.now()
+        if self.status=='approved' and self.approved_at is None:
+            self.approved_at = timezone.now()
+
+        super().save(*args, **kwargs) #Saves the database
+
+    def can_edit(self):
+        return self.status =='draft'
+    def can_submit(self):
+        return self.status=='draft'
+    def can_review(self):
+        return self.status=='submitted'
+    def can_approve(self):
+        return self.status=='reviewed'
 
 class SupervisorReview(models.Model):
     log = models.OneToOneField(WeeklyLog, 
