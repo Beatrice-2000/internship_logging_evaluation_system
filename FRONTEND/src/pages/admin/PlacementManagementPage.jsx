91dfe3dac5267d40a44d7e0fd8839d8bec991aef
kaladi-esther <estherkaladdi@gmail.com>
2026-04-28 15:45:39 +0300

    Update PlacementManagementPage.jsx

--- FRONTEND/src/pages/admin/PlacementManagementPage.jsx
+++ FRONTEND/src/pages/admin/PlacementManagementPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import API from "../Services/Api";
+import Api from "../Services/Api";
 
 const PlacementManagementPage = () => {
   const [placements, setPlacements] = useState([]);
