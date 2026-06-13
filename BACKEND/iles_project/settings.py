6bc425fce8177dd16270e3a643fee2acefcd076e
kkenan26 <kkenan2420@gmail.com>
2026-06-13 15:02:36 +0300

    Final deployed changes

--- BACKEND/iles_project/settings.py
+++ BACKEND/iles_project/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
