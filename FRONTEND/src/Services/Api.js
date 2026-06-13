62dde2197e9d1d4fb44fd72fe16c57113c78ef36
kkenan26 <kkenan2420@gmail.com>
2026-06-13 11:48:32 +0300

    Change base url

--- FRONTEND/src/Services/Api.js
+++ FRONTEND/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://iles-backend-sdd4.onrender.com/',
 });
 
 // Attach access token to every request
